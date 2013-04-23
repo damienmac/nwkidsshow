125d28ef846c9756db42e77879f266dc56f6ebda
Damien Macielinski <damienmac@yahoo.com>
2013-04-22 21:24:38 -0700

    use the right sqlite.db file

--- settings.py
+++ settings.py
@@ -13,7 +13,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Users\\Damien\\workspace\\nwkidsshow\\sqlite.db',                      # Or path to database file if using sqlite3.
+        # 'NAME': 'C:\\Users\\Damien\\workspace\\nwkidsshow\\sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'C:\\Users\\Damien\\PycharmProjects\\nwkidsshow\\sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
